7d2602463a5787e23329bdfe9e20cac8b2ef1ead
Joe Sak <joe@joesak.com>
2017-01-31 13:09:53 -0600

    Add localhost:8000 to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'joemsak.github.io', 'midtermcountdown.co', 'swordmaker-rat-27862.bitballoon.com'
+    origins 'localhost:3000', 'localhost:8000', 'joemsak.github.io', 'midtermcountdown.co', 'swordmaker-rat-27862.bitballoon.com'
 
     resource '*',
       headers: :any,
